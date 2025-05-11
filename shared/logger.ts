65476de372604944ca5caec28c285f2875b8c32a
zyj <zyjared@outlook.com>
2025-05-11 18:11:32 +0800

    Update logger.ts

--- shared/logger.ts
+++ shared/logger.ts
@@ -15,8 +15,8 @@ const LOG_LEVELS: Record<LogLevel, number> = {
 class Logger {
   private level: LogLevel = 'info'
 
-  setLevel(level: LogLevel) {
-    this.level = level
+  setLevel(level: LogLevel): void {
+    this.level = Object.hasOwn(LOG_LEVELS, level) ? level : 'info'
   }
 
   private shouldLog(level: LogLevel): boolean {
@@ -30,57 +30,58 @@ class Logger {
 
   private formatMessage(level: LogLevel, messages: any[]): string {
     // const timestamp = this.getTimestamp()
-    const prefix = this.getPrefix(level)
+    const fixer = messages.length > 1 ? ` ${messages.shift()}` : ''
+    const prefix = this.getPrefix(level, fixer)
     const formattedMessages = messages.map(msg =>
       typeof msg === 'string' ? msg : inspect(msg, { depth: null, colors: true }),
     )
     return `${prefix} ${formattedMessages.join(' ')}`
   }
 
-  private getPrefix(level: LogLevel): string {
+  private getPrefix(level: LogLevel, fixer = ''): string {
     switch (level) {
       case 'error':
-        return bold(red('✗'))
+        return bold(red(`✗${fixer}`))
       case 'warn':
-        return bold(yellow('!'))
+        return bold(yellow(`!${fixer}`))
       case 'info':
-        return bold(blue('i'))
+        return bold(blue(`i${fixer}`))
       case 'success':
-        return bold(green('✓'))
+        return bold(green(`✓${fixer}`))
       case 'debug':
-        return bold(blue('🔍'))
+        return bold(blue(`🔍${fixer}`))
     }
   }
 
-  error(...messages: any[]) {
+  error(...messages: any[]): void {
     if (this.shouldLog('error'))
       console.error(this.formatMessage('error', messages))
   }
 
-  warn(...messages: any[]) {
+  warn(...messages: any[]): void {
     if (this.shouldLog('warn'))
       console.warn(this.formatMessage('warn', messages))
   }
 
-  info(...messages: any[]) {
+  info(...messages: any[]): void {
     if (this.shouldLog('info'))
       // eslint-disable-next-line no-console
       console.log(this.formatMessage('info', messages))
   }
 
-  success(...messages: any[]) {
+  success(...messages: any[]): void {
     if (this.shouldLog('success'))
       // eslint-disable-next-line no-console
       console.log(this.formatMessage('success', messages))
   }
 
-  debug(...messages: any[]) {
+  debug(...messages: any[]): void {
     if (this.shouldLog('debug'))
       // eslint-disable-next-line no-console
       console.log(this.formatMessage('debug', messages))
   }
 
-  title(message: string, ...msgs: any[]) {
+  title(message: string, ...msgs: any[]): void {
     // eslint-disable-next-line no-console
     console.log(`${bold(magenta(`📌 ${message}`))}\n`, ...msgs)
   }
@@ -88,5 +89,22 @@ class Logger {
 
 const logger = new Logger()
 
-export const { error, warn, info, success, debug, title, setLevel } = logger
+// export const { error, warn, info, success, debug, title, setLevel } = logger
+export const error = logger.error.bind(logger)
+export const warn = logger.warn.bind(logger)
+export const info = logger.info.bind(logger)
+export const success = logger.success.bind(logger)
+export const debug = logger.debug.bind(logger)
+export const title = logger.title.bind(logger)
+export const setLevel = logger.setLevel.bind(logger)
+
+export const colors = {
+  red,
+  green,
+  blue,
+  yellow,
+  bold,
+  magenta,
+}
+
 export default logger
